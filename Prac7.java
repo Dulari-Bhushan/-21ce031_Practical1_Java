06004baf54701ad358ec0c53db607c68b5df7897
Dulari-Bhushan <110349969+Dulari-Bhushan@users.noreply.github.com>
2022-07-31 23:50:00 +0530

    Add files via upload

---
+++ Prac7.java
@@ -0,0 +1,25 @@
+package Practical1;
+
+import java.util.Scanner;
+
+public class Prac7 {
+    public static void main(String[] args) {
+
+        int a = 0;
+        final String string = " ";
+        for (int i = 0; i <= 7; i++) {
+            for (int j = 1; j <= 7 - i; j++) {
+                System.out.printf("%4s", string);
+            }
+            for (int c = 0; c <= i; c++) {
+                a = (int) Math.pow(2, c);
+                System.out.printf("%4d", a);
+            }
+            for (int c = i - 1; c >= 0; c--) {
+                a = (int) Math.pow(2, c);
+                System.out.printf("%4d", a);
+            }
+            System.out.println();
+        }
+    }
+}
