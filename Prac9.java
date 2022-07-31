79dac404712fb5c9f26d89495377192d1b356d33
Dulari-Bhushan <110349969+Dulari-Bhushan@users.noreply.github.com>
2022-07-31 23:53:47 +0530

    Add files via upload

---
+++ Prac9.java
@@ -0,0 +1,27 @@
+package Practical1;
+
+import java.util.Scanner;
+
+public class Prac9 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] d = new int[9][9];
+        int temp = 0;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                d[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                temp = temp ^ d[i][j];
+            }
+        }
+        if (temp == 1)
+            System.out.println("Yes It's possible");
+        else
+            System.out.println("No It isn't possible");
+
+    }
+}
