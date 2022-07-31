413d256602adf9a4f0e8104e26d5fd02d72e0e66
Dulari-Bhushan <110349969+Dulari-Bhushan@users.noreply.github.com>
2022-07-31 23:56:24 +0530

    Add files via upload

---
+++ Pract10.java
@@ -0,0 +1,29 @@
+package Practical1;
+
+import java.util.Scanner;
+
+public class Pract10 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String string = sc.next();
+        int length = string.length();
+
+        char[] sh = string.toCharArray();
+
+        System.out.print("Enter digits to be shifted");
+        int pos = sc.nextInt();
+        for (int i = 0; i < length; i++) {
+
+            sh[i] = (char) (sh[i] + (pos));
+
+        }
+        System.out.println("The cipher code respectively after shifting:");
+        for (int i = 0; i < length; i++) {
+
+            System.out.print(sh[i]);
+        }
+
+    }
+}
